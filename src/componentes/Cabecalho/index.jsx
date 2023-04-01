ec2deb30606e0f2fdd306366b657d1473c569bb7
Israel Pereira <israel.pereira@dcx.ufpb.br>
2023-04-01 13:15:12 -0300

    criando rodape com components

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -5,7 +5,7 @@ import styles from './Cabecalho.module.scss'
 export default function Cabecalho(){
   return (
     <header className={styles.cabecalho}>
-      <img src={logo} alt="Logo do Alura Space" />
+      <img src={logo} alt="Logo do Space" />
       <div className={styles.cabecalho__container}>
         <input className={styles.cabecalho__input} type="text"
         placeholder="O que você procura?" />
